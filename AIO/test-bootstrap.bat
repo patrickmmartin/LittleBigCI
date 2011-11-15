4f847a975cb92de6939285c57855d3d19b67174f
Patrick Martin <patrickmmartin@gmail.com>
2011-11-15 00:20:20 +0000

    adding revisions as demoed at the GTAC 2008 talk

---
+++ AIO/test-bootstrap.bat
@@ -0,0 +1,38 @@
+@echo off
+if exist %TEMP%\CI (
+  echo %%TEMP%%\CI exists - removing %%TEMP%%\CI
+  rmdir /s / q %TEMP%\CI
+)
+
+schtasks /delete /F /TN post-revs
+schtasks /delete /F /TN build-revs
+schtasks /delete /F /TN test-revs
+
+echo ############### testing bootstrap
+call bootstrap %TEMP%\CI /USER
+echo ############### finished bootstrap
+if errorlevel 1 echo bootstrap failed! & exit /b 
+
+:: unit tests :-(
+
+svnadmin verify %TEMP%\CI\repo 
+if errorlevel 1 (
+  set SCRIPTERROR=repo creation failed & goto :testerror 
+) else echo repo creation succeeded
+if not exist %TEMP%\CI\scripts (
+  set SCRIPTERROR=scripts folder creation failed & goto :testerror
+) else echo scripts folder OK
+if not exist %TEMP%\CI\build-appliance (
+  set SCRIPTERROR=build-appliance folder creation failed & goto :testerror
+) else echo scripts folder OK
+if not exist %TEMP%\CI\test-appliance (
+  set SCRIPTERROR=test-appliance folder creation failed & goto :testerror
+) else echo test-appliance folder OK
+
+goto :eof
+
+:testerror
+
+echo %SCRIPTERROR%
+
+exit /b 1
